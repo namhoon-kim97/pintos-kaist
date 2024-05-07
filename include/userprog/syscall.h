a9b7b73c0ad4e09a4e13bb2b7d4a87dcd2b9e7f1
Ubuntu <ubuntu@ip-172-31-2-119.ap-northeast-2.compute.internal>
2024-05-07 10:56:12 +0000

    feat: syscall(write)

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -2,12 +2,6 @@
 #define USERPROG_SYSCALL_H
 
 void syscall_init(void);
-
-void halt();
 void exit(int status);
-_Bool create(const char *file, unsigned initial_size);
-_Bool remove(const char *file);
-int open(const char *file);
-void close(int fd);
-
+void check_address(void *addr);
 #endif /* userprog/syscall.h */
